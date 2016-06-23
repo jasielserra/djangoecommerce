2cde09bfb271792537e8442d13de898953dd9a71
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-22 22:31:08 -0300

    Criado pacotes de tests e o test da view index

---
+++ djangoecommerce/core/tests/test_view_index.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+class IndexTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        '''GET / must return status code 200.'''
+        self.assertEquals(200, self.resp.status_code)
+
+    def test_template(self):
+        '''Must use index.html'''
+        self.assertTemplateUsed(self.resp, 'index.html')
