2bc81a63e1d373657d8a24d2fc03220072562a29
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-22 22:42:50 -0300

    Teste da view contact

--- djangoecommerce/core/tests/test_view_product.py
+++ djangoecommerce/core/tests/test_view_product.py
@@ -1,13 +1,13 @@
-from  django.test import TestCase
+from django.test import TestCase
 
 class ContactTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/contato/')
+        self.resp = self.client.get('/produto/')
 
     def test_get(self):
         '''GET / must return status code 200.'''
         self.assertEquals(200, self.resp.status_code)
 
     def test_template(self):
-        '''Must use contact.html'''
-        self.assertTemplateUsed(self.resp, 'contact.html')
+        '''Must use product.html'''
+        self.assertTemplateUsed(self.resp, 'product.html')
