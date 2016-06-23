63b46125b9e0fae96ff700e2d71d7a0da9d3c4b0
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-22 22:01:46 -0300

    Definicao das views

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def product_list(request):
+    return render(request,'product_list.html')
+
+def product(request):
+    return render(request,'product.html')
