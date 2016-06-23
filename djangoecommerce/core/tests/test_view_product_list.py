4c3c8bf28257c7418ad7e599f92acdde36c2a119
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-22 22:48:01 -0300

    Teste da view product_list

--- djangoecommerce/core/tests/test_view_product_list.py
+++ djangoecommerce/core/tests/test_view_product_list.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 
-class ProductTest(TestCase):
+class ProductListTest(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/produto/')
+        self.resp = self.client.get('/produtos/')
 
     def test_get(self):
         '''GET / must return status code 200.'''
         self.assertEquals(200, self.resp.status_code)
 
     def test_template(self):
-        '''Must use product.html'''
-        self.assertTemplateUsed(self.resp, 'product.html')
+        '''Must use products_list.html'''
+        self.assertTemplateUsed(self.resp, 'product_list.html')
