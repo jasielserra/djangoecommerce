8741aafc2727474991865a91be1a1bae1baee125
Jasiel Serra <jasiel_serra@hotmail.com>
2016-07-04 00:27:44 -0300

    Personalizado Model CategoryAdmin

--- djangoecommerce/catalog/admin.py
+++ djangoecommerce/catalog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at', 'modified']
+    search_fields = ['name', 'slug']
+    list_filter = ['created_at', 'modified']
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
