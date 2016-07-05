6b81320107ea8f15a515b611459772ef3b89f356
Jasiel Serra <jasiel_serra@hotmail.com>
2016-07-05 00:33:48 -0300

    Adiciona Category ao contexto da view index

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from djangoecommerce.catalog.models import Category
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    context = {'categories':Category.objects.all()}
+    return render(request,'index.html', context)
 
 def contact(request):
     return render(request,'contact.html')
