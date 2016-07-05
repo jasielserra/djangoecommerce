c1c195a59dea5db239f7857ba5af9860bd9d35ba
Jasiel Serra <jasiel_serra@hotmail.com>
2016-07-05 00:30:46 -0300

    Realizado teste de regressão da view e do template contact

--- djangoecommerce/core/tests/test_view_contact.py
+++ djangoecommerce/core/tests/test_view_contact.py
@@ -1,13 +1,26 @@
-from  django.test import TestCase
+from django.test import TestCase
 
 class ContactTest(TestCase):
     def setUp(self):
         self.resp = self.client.get('/contato/')
 
     def test_get(self):
-        '''GET / must return status code 200.'''
-        self.assertEquals(200, self.resp.status_code)
+        '''GET /Contact/ must return status code 200.'''
+        self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
         '''Must use contact.html'''
         self.assertTemplateUsed(self.resp, 'contact.html')
+
+    def test_html(self):
+        '''HTML must contains input tags.'''
+        tags = (('<form',1),
+               ('<input',2),
+                ('type="text"',2),
+                ('type="submit"',1))
+
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.resp, text, count)
+
+
