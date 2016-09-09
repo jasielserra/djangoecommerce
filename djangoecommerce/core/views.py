0e2c5d487c23592411aab2ed061bd7d56cecbec3
Jasiel Serra <jasiel_serra@hotmail.com>
2016-09-09 00:37:41 -0300

    Implementando Envio de e-mail atraves Console Emailbackend

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.conf import settings
+from django.core.mail import send_mail
 
-# Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def contact(request):
+    success = False
     if request.method == 'POST':
         form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            message = 'Nome: {0}\nE-mail:{1}\n{2}'.format(name,email,message)
+            send_mail(
+                'Contato do Django E-Commerce', message, settings.DEFAULT_FROM_EMAIL,
+                [settings.DEFAULT_FROM_EMAIL]
+            )
+            success = True
+
     else:
         form = ContactForm()
+
     context = {
-        'form':form
+        'form':form,
+        'success':success
     }
     return render(request,'contact.html', context)
 
--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -29,6 +29,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+DEFAULT_FROM_EMAIL = 'contato@mailinator.com'
+
 
 # Application definition
 
@@ -128,3 +130,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+#Email configuration
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
