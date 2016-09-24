8409e37534497ac9d868f0aa8a68b735e8d13e8f
Jasiel Serra <jasiel_serra@hotmail.com>
2016-09-24 00:43:27 -0300

    Category as Class-Based_View ListView

--- djangoecommerce/catalog/views.py
+++ djangoecommerce/catalog/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.views import generic
+
 
 class ProductListView(generic.ListView):
     model = Product
     template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
 
 product_list = ProductListView.as_view()
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {'current_category':category,
-               'products': Product.objects.filter(category=category),
-               }
-    return render(request,'catalog/category.html', context)
+class CategoryListView(generic.ListView):
+    template_name = 'catalog/category.html'
+    context_object_name = 'products'
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+category = CategoryListView.as_view()
 
 def product(request, slug):
     product = Product.objects.get(slug=slug)
     context = {'product': product}
     return render(request, 'catalog/product.html', context)
+
+
+
+
+
+
