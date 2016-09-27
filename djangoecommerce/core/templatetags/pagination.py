5fa7fd3a73598bd3ee92ee95bef3cb405d9fa71c
Jasiel Serra <jasiel_serra@hotmail.com>
2016-09-27 00:55:11 -0300

    Implementa a lógica da custom template tag que mantem a query string

--- djangoecommerce/core/templatetags/pagination.py
+++ djangoecommerce/core/templatetags/pagination.py
@@ -8,4 +8,11 @@ def pagination(request, paginator, page_obj):
     context['paginator'] = paginator
     context['request'] = request
     context['page_obj'] = page_obj
+    getvars = request.GET.copy()
+    if 'page' in getvars:
+        del getvars['page']
+    if len(getvars) > 0:
+        context['getvars'] = '&{0}'.format(getvars.urlenconde())
+    else:
+        context['getvars'] = ''
     return context
