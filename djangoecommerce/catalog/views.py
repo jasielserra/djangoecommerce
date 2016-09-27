f6cb68f28c1787095ad996cfd687f164e86f2ec6
Jasiel Serra <jasiel_serra@hotmail.com>
2016-09-26 23:50:40 -0300

    Implementa Paginacao

--- djangoecommerce/catalog/views.py
+++ djangoecommerce/catalog/views.py
@@ -7,12 +7,14 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'catalog/product_list.html'
     context_object_name = 'products'
+    paginate_by = 3
 
 product_list = ProductListView.as_view()
 
 class CategoryListView(generic.ListView):
     template_name = 'catalog/category.html'
     context_object_name = 'products'
+    paginate_by = 3
     def get_queryset(self):
         return Product.objects.filter(category__slug=self.kwargs['slug'])
 
