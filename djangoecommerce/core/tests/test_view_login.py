5169214678cc190251f5779134b7c9f6dc554249
Jasiel Serra <jasielserra@hotmail.com>
2016-12-27 23:49:19 -0300

    Testing Login View valid and invalid

---
+++ djangoecommerce/core/tests/test_view_login.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+from model_mommy import mommy
+
+
+class LoginTestView(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('login'))
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_get(self):
+        '''GET / must return status code 200.'''
+        self.assertEquals(200, self.resp.status_code)
+
+    def test_template(self):
+        '''Must use login.html'''
+        self.assertTemplateUsed(self.resp, 'login.html')
+
+class ValidloginTest(TestCase):
+    def setUp(self):
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+        data = dict(username=self.user.username, password=123)
+        self.resp = self.client.post(r('login'), data)
+
+    def test_validlogin(self):
+        redirect_url = reverse(settings.LOGIN_REDIRECT_URL)
+        self.assertRedirects(self.resp, redirect_url)
+
+    def test_user_authenticated(self):
+        self.assertTrue(self.resp.wsgi_request.user.is_authenticated())
+
+class InValidloginTest(TestCase):
+    def setUp(self):
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+        data = dict(username=self.user.username, password=1234)
+        self.resp = self.client.post(r('login'), data)
+
+    def test_get(self):
+        '''GET / must return status code 200.'''
+        self.assertEquals(200, self.resp.status_code)
+
+    def test_template(self):
+        '''Must use login.html'''
+        self.assertTemplateUsed(self.resp, 'login.html')
+
+    def test_invalidlogin(self):
+        error_msg = ('Por favor, entre com um usuário  e senha corretos.'
+                     ' Note que ambos os campos diferenciam maiúsculas e minúsculas.')
+        self.assertFormError(self.resp, 'form', None, error_msg)
--- djangoecommerce/core/tests/test_view_register.py
+++ djangoecommerce/core/tests/test_view_register.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class IndexTest(TestCase):
+
+class RegisterTest(TestCase):
     def setUp(self):
         self.resp = self.client.get(r('register'))
 
@@ -13,3 +17,15 @@ class IndexTest(TestCase):
     def test_template(self):
         '''Must use index.html'''
         self.assertTemplateUsed(self.resp, 'register.html')
+
+class RegisterPostValid(TestCase):
+    def setUp(self):
+        data = dict(username='jasi', password1='teste123', password2='teste123')
+        self.resp = self.client.post(r('register'), data)
+
+    def test_post(self):
+        """ Valid POST should redirect to /index/ """
+        self.assertRedirects(self.resp, r('index'))
+
+    def test_quantity(self):
+        self.assertEquals(User.objects.count(), 1)
