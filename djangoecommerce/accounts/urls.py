c525bbce3a8d4dd295d0b1b2d529f512ee046efd
Jasiel Serra <jasiel_serra@hotmail.com>
2017-01-12 22:40:13 -0300

    Implementa UpdateUserView

--- djangoecommerce/accounts/urls.py
+++ djangoecommerce/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from djangoecommerce.accounts.views import register, index
+from djangoecommerce.accounts.views import register, index, update_user
 
 urlpatterns = [
         url(r'^$', index, name='index'),
+        url(r'^alterar-dados/$', update_user, name='update_user'),
         url(r'^registro/$', register, name='register'),
         ]
--- djangoecommerce/accounts/views.py
+++ djangoecommerce/accounts/views.py
@@ -1,14 +1,12 @@
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from djangoecommerce.accounts.forms import UserAdminCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from .models import User
 
 class IndexView(LoginRequiredMixin, TemplateView):
 
     template_name = 'accounts/index.html'
-
 
 class RegisterView(CreateView):
     model = User
@@ -16,6 +14,18 @@ class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
+class UpdateUserView(LoginRequiredMixin,UpdateView):
+
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name','email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+
+update_user = UpdateUserView.as_view()
 index = IndexView.as_view()
 register = RegisterView.as_view()
 
