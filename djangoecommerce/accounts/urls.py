d460ca1676b22c86ddfbd9741a3dd4e6804d8361
Jasiel Serra <jasiel_serra@hotmail.com>
2017-01-12 23:33:17 -0300

    Update Password View

--- djangoecommerce/accounts/urls.py
+++ djangoecommerce/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from djangoecommerce.accounts.views import register, index, update_user
+from djangoecommerce.accounts.views import register, index, update_user, update_password
 
 urlpatterns = [
         url(r'^$', index, name='index'),
         url(r'^alterar-dados/$', update_user, name='update_user'),
+        url(r'^alterar-senha/$', update_password, name='update_password'),
         url(r'^registro/$', register, name='register'),
         ]
--- djangoecommerce/accounts/views.py
+++ djangoecommerce/accounts/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 from django.core.urlresolvers import reverse_lazy
 from djangoecommerce.accounts.forms import UserAdminCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import PasswordChangeForm
 from .models import User
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -25,7 +26,18 @@ class UpdateUserView(LoginRequiredMixin,UpdateView):
         return self.request.user
 
 
+class UpdatePasswordView(LoginRequiredMixin,FormView):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView,self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
 update_user = UpdateUserView.as_view()
 index = IndexView.as_view()
 register = RegisterView.as_view()
-
+update_password = UpdatePasswordView.as_view()
