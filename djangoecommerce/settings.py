a6ffb1d4a707587b465d8f8d179b3a612f8c104a
Jasiel Serra <jasiel_serra@hotmail.com>
2017-01-17 22:37:20 -0300

    Enable Email Backend

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -134,12 +134,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 #Email configuration
 
-#EMAIL_BACKEND = config('EMAIL_BACKEND')
-#EMAIL_HOST = config('EMAIL_HOST')
-#EMAIL_PORT = config('EMAIL_PORT', cast=int)
-#EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 #auth
 LOGIN_URL = 'login'
