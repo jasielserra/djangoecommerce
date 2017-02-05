515a8ad3d6863475b8f3ba5799a569ee4f7fd035
Jasiel Serra <jasiel_serra@hotmail.com>
2017-02-05 15:57:05 -0300

    Remove item of the Cart when quantity == 0

--- djangoecommerce/checkout/views.py
+++ djangoecommerce/checkout/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import RedirectView, TemplateView
-from djangoecommerce.catalog.models import Product
-from .models import CartItem
-from django.forms import modelformset_factory
+# coding=utf-8
+
 from django.shortcuts import get_object_or_404
+from django.views.generic import RedirectView, TemplateView
+from django.forms import modelformset_factory
 from django.contrib import messages
 from django.core.urlresolvers import reverse
+
+from djangoecommerce.catalog.models import Product
+
+from .models import CartItem
 
 
 class CreateCartItemView(RedirectView):
@@ -14,11 +17,13 @@ class CreateCartItemView(RedirectView):
         product = get_object_or_404(Product, slug=self.kwargs['slug'])
         if self.request.session.session_key is None:
             self.request.session.save()
-        cart_item, created = CartItem.objects.add_item(self.request.session.session_key, product)
+        cart_item, created = CartItem.objects.add_item(
+            self.request.session.session_key, product
+        )
         if created:
-            messages.success(self.request,'Produto Adicionado com Sucesso!!')
+            messages.success(self.request, 'Produto adicionado com sucesso')
         else:
-            messages.success(self.request,'Produto Atualizado com Sucesso!!')
+            messages.success(self.request, 'Produto atualizado com sucesso')
         return reverse('checkout:cart_item')
 
 
@@ -26,16 +31,38 @@ class CartItemView(TemplateView):
 
     template_name = 'checkout/cart.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(CartItemView, self).get_context_data(**kwargs)
-        CartItemFormSet = modelformset_factory(CartItem, fields=('quantity',), can_delete=True, extra=0)
+    def get_formset(self, clear=False):
+        CartItemFormSet = modelformset_factory(
+            CartItem, fields=('quantity',), can_delete=True, extra=0
+        )
         session_key = self.request.session.session_key
         if session_key:
-            context['formset'] = CartItemFormSet(queryset=CartItem.objects.filter(cart_key=session_key))
+            if clear:
+                formset = CartItemFormSet(
+                    queryset=CartItem.objects.filter(cart_key=session_key)
+                )
+            else:
+                formset = CartItemFormSet(
+                    queryset=CartItem.objects.filter(cart_key=session_key),
+                    data=self.request.POST or None
+                )
         else:
-            context['formset'] = CartItemFormSet(queryset=CartItem.objects.none())
+            formset = CartItemFormSet(queryset=CartItem.objects.none())
+        return formset
+
+    def get_context_data(self, **kwargs):
+        context = super(CartItemView, self).get_context_data(**kwargs)
+        context['formset'] = self.get_formset()
         return context
 
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        context = self.get_context_data(**kwargs)
+        if formset.is_valid():
+            formset.save()
+            messages.success(request, 'Carrinho atualizado com sucesso')
+            context['formset'] = self.get_formset(clear=True)
+        return self.render_to_response(context)
 
 
 create_cartitem = CreateCartItemView.as_view()
