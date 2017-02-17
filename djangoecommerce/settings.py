b43ca1636d6240191aba54f0c5982dd821e0e85b
Jasiel Serra <jasiel_serra@hotmail.com>
2017-02-16 23:13:14 -0300

    Implementa teste da App Checkout

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
-    'django.whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'djangoecommerce.checkout.middleware.cart_item_middleware',
+    'checkout.middleware.cart_item_middleware',
 ]
 
 ROOT_URLCONF = 'djangoecommerce.urls'
